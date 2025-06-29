86558de04a0e8bf3f1fdee1e435c0e81e6dee868
LuceDeuce <124011783+lucibee@users.noreply.github.com>
2025-06-28 22:06:50 -0400

    boat workaround

--- src/main/java/com/perfectparitypg/entity/ModEntityRenderer.java
+++ src/main/java/com/perfectparitypg/entity/ModEntityRenderer.java
@@ -3,9 +3,13 @@ package com.perfectparitypg.entity;
 import com.perfectparitypg.entity.monster.CreakingRenderer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
+// Custom renderer for Pale Oak Boat
+import com.perfectparitypg.entity.PaleOakBoatRenderer;
+
 public class ModEntityRenderer {
     public static void registerEntityRenderers () {
         EntityRendererRegistry.register(ModEntities.CREAKING, CreakingRenderer::new);
-
+        EntityRendererRegistry.register(ModEntities.PALE_OAK_BOAT, ctx -> new PaleOakBoatRenderer(ctx, false));
+        EntityRendererRegistry.register(ModEntities.PALE_OAK_CHEST_BOAT, ctx -> new PaleOakBoatRenderer(ctx, true));
     }
 }
